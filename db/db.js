877535535664d173c1ccdf231dc2af8bb0079cd7
Robert Morais <bitmoraes@hotmail.com>
2021-05-24 19:53:04 -0300

    feat: deleção de fornecedor e correção de redirecionamento

--- db/db.js
+++ db/db.js
@@ -3,12 +3,10 @@ var mysql = require('mysql');
 var conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123456',
+    password: 'example',
     database: 'empresa',
     multipleStatements: true,
 });
 conexao.connect();
-
-console.log('Conectado ao bd, acesse: http://localhost:3000/empresa');
 
 module.exports = conexao;
